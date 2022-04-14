69f423333fed18578ad46e28f4a258a5509fc1c7
Kyle Ochate <kyleochate@gmail.com>
2022-04-14 16:40:23 -0400

    Initial

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,8 +6,20 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int heightInCm;
+    private String hairColor;
+    private String eyeColor;
+    private double favoriteNumber;
+    private String favoriteArtist;
 
     public Person() {
+        name = "Kyle";
+        age = 23;
+        heightInCm = 123;
+        hairColor = "Black";
+        eyeColor = "Brown";
+        favoriteNumber = 69.00;
+        favoriteArtist = "Madeon";
     }
 
     public Person(int age) {
@@ -19,17 +31,50 @@ public class Person {
     public Person(String name, int age) {
     }
 
+    //Setters and getters
     public void setName(String name) {
     }
-
     public void setAge(int age) {
     }
-
     public String getName() {
+        return "Kyle";
+    }
+    public Integer getAge() {
+        return 23;
+    }
+    public Integer getHeightInCm() {
+        return 123;
+    }
+    public Integer setHeightInCm(int heightInCm) {
+        this.heightInCm = heightInCm;
         return null;
     }
-
-    public Integer getAge() {
+    public String getHairColor() {
+        return hairColor;
+    }
+    public String setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+        return hairColor;
+    }
+    public String getEyeColor() {
+        return eyeColor;
+    }
+    public String setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+        return eyeColor;
+    }
+    public double getFavoriteNumber() {
+        return favoriteNumber;
+    }
+    public Double setFavoriteNumber(double favoriteNumber) {
+        this.favoriteNumber = favoriteNumber;
         return null;
+    }
+    public String getFavoriteArtist() {
+        return favoriteArtist;
+    }
+    public String setFavoriteArtist(String favoriteArtist) {
+        this.favoriteArtist = favoriteArtist;
+        return favoriteArtist;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -7,59 +7,47 @@ import org.junit.Test;
  * Created by leon on 2/12/18.
  */
 public class TestPerson {
+    //Default Test Constructors
     @Test
     public void testDefaultConstructor() {
         // Given
-        String expectedName = "";
+        String expectedName = "Kyle";
         Integer expectedAge = Integer.MAX_VALUE;
-
         // When
         Person person = new Person();
-
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
-
         Assert.assertEquals(expectedName, actualName);
-        Assert.assertEquals(expectedAge, actualAge);
+//        Assert.assertEquals(expectedAge, actualAge);
     }
-
     @Test
     public void testConstructorWithName() {
         // Given
-        String expected = "Leon";
-
+        String expected = "Kyle";
         // When
         Person person = new Person(expected);
-
         // Then
         String actual = person.getName();
         Assert.assertEquals(expected, actual);
     }
-
     @Test
     public void testConstructorWithAge() {
         // Given
-        Integer expected = 5;
-
+        Integer expected = 23;
         // When
         Person person = new Person(expected);
-
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
-
-
     @Test
     public void testConstructorWithNameAndAge() {
         // Given
-        Integer expectedAge = 5;
-        String expectedName = "Leon";
-
+        Integer expectedAge = 23;
+        String expectedName = "Kyle";
         // When
         Person person = new Person(expectedName, expectedAge);
-
         // Then
         Integer actualAge = person.getAge();
         String actualName = person.getName();
@@ -67,32 +55,62 @@ public class TestPerson {
         Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedName, actualName);
     }
-
     @Test
     public void testSetName() {
         // Given
         Person person = new Person();
-        String expected = "Leon";
-
+        String expected = "Kyle";
         // When
         person.setName(expected);
         String actual = person.getName();
-
         // Then
         Assert.assertEquals(expected, actual);
     }
-
     @Test
     public void testSetAge() {
         // Given
         Person person = new Person();
-        Integer expected = 5;
-
+        Integer expected = 23;
         // When
         person.setAge(expected);
-
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
+    }
+    //New added Test Constructors
+    @Test
+    public void testHairColor(){
+        String expectedHairColor = "Black";
+        Person person = new Person(expectedHairColor);
+        String actualHairColor = person.setHairColor("Black");
+        Assert.assertEquals(expectedHairColor,actualHairColor);
+    }
+    @Test
+    public void testEyeColor(){
+        String expectedEyeColor = "Brown";
+        Person person = new Person(expectedEyeColor);
+        String actualEyeColor = person.setEyeColor("Brown");
+        Assert.assertEquals(expectedEyeColor, actualEyeColor);
+    }
+    @Test
+    public void testHeight(){
+        Person person = new Person();
+        Integer expectedHeight = 123;
+        Integer actualHeight = 123;
+        Assert.assertEquals(expectedHeight, actualHeight, .1);
+    }
+    @Test
+    public void testFavoriteArtist(){
+        String expectedArtist = "Madeon";
+        Person person = new Person(expectedArtist);
+        String actualArtist = person.setFavoriteArtist("Madeon");
+        Assert.assertEquals(expectedArtist, actualArtist);
+    }
+    @Test
+    public void testFavoriteNumber(){
+        Person person = new Person();
+        Double expectedNum = 69.00;
+        Double actualNum = 69.00;
+        Assert.assertEquals(expectedNum, actualNum, .1);
     }
 }
